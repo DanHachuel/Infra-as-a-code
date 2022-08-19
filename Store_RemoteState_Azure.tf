63b8d85992706176eef355e38c8f7727ab887b8a
Dan Pariente Hachuel <61590061+DanHachuel@users.noreply.github.com>
2022-08-19 13:36:49 -0300

    Create Store_RemoteState_Azure.tf

---
+++ Store_RemoteState_Azure.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
