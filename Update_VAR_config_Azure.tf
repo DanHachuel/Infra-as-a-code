f6ee24f904c600cb0e4e3441e3eff39c3c96bf22
Dan Pariente Hachuel <61590061+DanHachuel@users.noreply.github.com>
2022-08-19 13:27:00 -0300

    Create Update_VAR_config_Azure.tf

---
+++ Update_VAR_config_Azure.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "westus2"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
+}
